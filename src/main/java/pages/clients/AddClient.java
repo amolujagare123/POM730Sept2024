f5a5bc0e695bf5bdf98bc66c24aa21bd26871c56
amolu <amolujagare@gmail.com>
2024-09-20 20:18:10 +0530

    pom

--- src/main/java/pages/clients/AddClient.java
+++ src/main/java/pages/clients/AddClient.java
@@ -1,4 +1,19 @@
 package pages.clients;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AddClient {
+
+    @FindBy(xpath="//input[@id='client_name']")
+    WebElement clientName;
+
+    @FindBy(xpath="//input[@id='client_surname']")
+    WebElement clientSurname;
+
+    @FindBy(xpath="//input[@id='client_address_1']")
+    WebElement streetAddress1;
+
+    @FindBy(xpath="//input[@id='client_address_2']")
+    WebElement streetAddress2;
 }
